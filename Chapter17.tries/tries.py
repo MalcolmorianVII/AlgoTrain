e68d4e880cc03c9c00c2c12bd7f6e9c54b395338
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2022-07-20 12:03:20 +0200

    autocorrect

--- Chapter17.tries/tries.py
+++ Chapter17.tries/tries.py
@@ -45,7 +45,39 @@ class Trie:
         if not currentNode:
             return None
         return self.collectWords(currentNode)
+    def trie_traverse(self,node=None):
+        current_node = node or self.root
+        for key,childNode in current_node.children.items():
+            print(key)
+            if key != "*":
+                self.trie_traverse(childNode)
+
+    def autocorrect(self,str,node=None,word=""):
+        current_node = node or self.root
+        if current_node == "*":
+            return word
+        for key,childNode in current_node.children.items():
+            if key == "*":
+                return word
+            else:
+                if str[0] in current_node.children.items():
+                    continue
+                else:
+                    return self.autocorrect(str[1:],childNode,word + key)
+        
+    
 
 rootNode = TrieNode()
-rootNode.children = {'a':TrieNode(),'b':TrieNode(),'c':TrieNode()}
-print(rootNode)
+node1 = TrieNode()
+node1.children = {'d':"*"}
+node2 = TrieNode()
+node2.children = {'a':"*"}
+node3 = TrieNode()
+node3.children = {'o':"*"}
+rootNode.children = {'a':node1,'b':node2,'c':node3}
+
+trie = Trie()
+trie.root = rootNode
+
+# trie.trie_traverse()
+print(trie.autocorrect('ct'))

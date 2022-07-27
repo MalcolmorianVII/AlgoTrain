7a35f52c02a67c96c48c1574c0fb5121bf08721c
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2022-07-27 20:50:36 +0200

    traverse a graph using dfs

--- Chapter19.Graphs/graphs.py
+++ Chapter19.Graphs/graphs.py
@@ -6,6 +6,18 @@ class Vertex:
     def add_adj_vertices(self,vertex):
         self.adj_vertices.append(vertex)
 
+def dfs_traverse(vertex,visited_vertices={}):
+    # Mark vertex as visited 
+    visited_vertices[vertex.value] = True
+
+    # print the vertex value
+    print(vertex.value)
+
+    for adj in vertex.adj_vertices:
+        if visited_vertices.get(adj.value):
+            continue
+        dfs_traverse(adj,visited_vertices)
+    
 alice = Vertex("Alice")
 bob = Vertex("Bob")
 cynthia = Vertex("Cynthia")
@@ -14,4 +26,6 @@ alice.add_adj_vertices(bob)
 alice.add_adj_vertices(cynthia)
 bob.add_adj_vertices(cynthia)
 cynthia.add_adj_vertices(bob)
+
+dfs_traverse(alice)
 

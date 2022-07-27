23a14569a514aac7868409984f3fd693b6b788f6
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2022-07-27 20:50:50 +0200

    golomb sequence

--- DynamicProg/exercise2.py
+++ DynamicProg/exercise2.py
@@ -3,12 +3,12 @@ def golomb(n):
         return 1
     return 1 + golomb( n - golomb(golomb(n-1)))
 
-def golomb1(n,mem = {}):
+def golomb(n,mem = {}):
     if n == 1:
         return 1
     
     if not mem.get(n):
-        mem[n] =  1 + golomb1( n - golomb1(golomb1(n-1,mem),mem),mem)
+        mem[n] =  1 + golomb( n - ((n-1,mem),mem),mem)
     return mem[n]
 
 print(golomb(2))

91a8c619c83046083024d2e5c100cc1b9b0f73c7
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2022-07-28 14:50:55 +0200

    bfs graph traverse

--- Chapter19.Graphs/graphs.py
+++ Chapter19.Graphs/graphs.py
@@ -1,4 +1,4 @@
-
+from queue import Queue
 class Vertex:
     def __init__(self,value,adj_vertices=[]) -> None:
         self.value = value
@@ -17,6 +17,22 @@ def dfs_traverse(vertex,visited_vertices={}):
         if visited_vertices.get(adj.value):
             continue
         dfs_traverse(adj,visited_vertices)
+
+def bfs_traverse(starting_vertex):
+    visited_matrices = {}
+    visited_matrices[starting_vertex.value] = True
+
+    queue = Queue()
+    queue.put(starting_vertex)
+
+    while not queue.empty():
+        current_vertex = queue.get()
+        print(current_vertex.value)
+        for adj in current_vertex.adj_vertices:
+            if visited_matrices.get(adj.value):
+                continue
+            visited_matrices[adj.value] = True
+            queue.put(adj)
     
 alice = Vertex("Alice")
 bob = Vertex("Bob")
@@ -27,5 +43,5 @@ alice.add_adj_vertices(cynthia)
 bob.add_adj_vertices(cynthia)
 cynthia.add_adj_vertices(bob)
 
-dfs_traverse(alice)
-
+# dfs_traverse(alice)
+bfs_traverse(alice)
